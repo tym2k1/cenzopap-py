41e6e2b3f4061db46e40d0fc86a2e1a238b7e380
tym2k1 <tymbur@gmail.com>
2022-12-07 02:40:18 +0100

    Now with fuzzy logic :)

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
 import re
+from fuzzywuzzy import fuzz
+import pandas as pd
+import numpy as np
 
-with open(os.path.join(os.path.dirname(__file__), 'word_blacklist.txt')) as file:
-   blacklist = set(file.read().split())                                                 #   wrzucone do seta bo najlepsze data type fo tego chyba + relative path!!!
-   #blacklist = set(line.strip() for line in file)                                      #   For large sets of words this option would be faster
+with open(os.path.join(os.path.dirname(__file__), 'word_blacklist1.txt')) as file:
+   blacklist = set(file.read().split('\n'))                                                 #   wrzucone do seta bo najlepsze data type fo tego chyba + relative path!!!
+   blacklist.discard('')
+   blacklist.discard(' ')
+   #blacklist = set(line.strip() for line in file)                                          #   For large blacklist this option would provide faster bootup
 
 replacement_dict = {
     'ą': 'a',
@@ -52,36 +57,63 @@ replacement_dict = {
     '[-': 'e',      # dokoncze potem
 }
 
+
+
 def translatetable(string):
     regex = re.compile('|'.join(map(re.escape, replacement_dict)))                      #   https://stackoverflow.com/questions/63230213/translate-table-value-error-valueerror-string-keys-in-translate-table-must-be-o
     return regex.sub(lambda match: replacement_dict[match.group(0)], string)            #   :)
 
-#def translatetable(string):
-    #string = string.translate(str.maketrans(replacement_dict))                         #   Works only for dict keys with a size of 1
-    #return string
-
-def removedup(string):
-    string = re.sub(r"(.)\1+", r"\1", string)                                           #   usuwanie tych samych literek kolo sb (chuuuuj = chuj)
-    return string                                                                       #   mozna zamienic na lambde
+def removedup(string):                                                                  #   usuwanie tych samych literek kolo sb (chuuuuj = chuj)
+    return re.sub(r"(.)\1+", r"\1", string)                                             #   mozna zamienic na lambde
 
 def inputtolist(string):
-    wordlist = re.findall(r'\w+', string)
-    return wordlist                                                                      #  mozna tez zamienic na lambde
+    return re.findall(r'\w+', string)                                                   #  mozna tez zamienic na lambde
 
-def cenzo(string):                                                                       #  main func poki co
-    #string = match.group()
-    if removedup(translatetable(string.lower())) in blacklist:
-        #return '*' * len(string)                                                         #  2 wersje zwracanie full cenzo (chuj = ****) albo niepelne (chuj = c**j)
-        return string[0] + '*' * (len(string)-2) + string[-1] 
+def lower(string):
+    return string.lower()
+
+
+
+def cenzo(string, int):                                                                      #  cenzurowanie slowek      (wrzucilem tutaj logike bo nie mam pojecia jak dac IF do zwektoryzowanej funkcji bez iterowania)
+    if int >= threshold:
+        return string[0] + '*' * (len(string)-2) + string[-1] #and True
     else:
-        return string
+        return string #and False
 
-print(translatetable('\/4p3'))
-print(translatetable('CH000000000i'))
-print(inputtolist("dupa maryna cyce wadowice"))
+def partial_match(x,y):
+    return(fuzz.ratio(x,y))
 
-in1 = "test dupa chuj gowno gówno pizda ogień jebać politechnikę gównianą pierdolic pięrdołić"
-out1 = []
-for i in inputtolist(in1):
-    out1.append(cenzo(i))
-print(out1)
+partial_match_vector = np.vectorize(partial_match)                                      #   https://stackoverflow.com/questions/56040817/python-fuzzy-matching-strings-in-list-performance
+partial_cenzo_vector = np.vectorize(cenzo)
+
+
+
+
+input = "Chuj chuj chuj chój chuja chuuuuuuuja chujek chuju chujem huj huja hujek huju hujem"
+output = []
+#inputlistvar = inputtolist(input)
+threshold = 75                                                                          #   w jakim procencie musza sie pokrywac by byc ocenzurowane (0-100)
+
+
+
+
+
+dataframecolumn_match = pd.DataFrame(inputtolist(removedup(translatetable(lower(input)))))
+dataframecolumn_original = pd.DataFrame(inputtolist(input))
+dataframecolumn_original.columns = ['Original']
+dataframecolumn_match.columns = ['Match']
+
+dataframecolumn_compare = pd.DataFrame(blacklist)
+dataframecolumn_compare.columns = ['Compare']                                           #   nie pytajcie do konca jak to wszystko dziala ale dziala SZYBKO czyli zajebiscie ale sie jebalem z tym fchuj
+
+dataframecolumn_original['Match'] = dataframecolumn_match
+dataframecolumn_original['Key'] = 1
+dataframecolumn_compare['Key'] = 1
+
+combined_dataframe = dataframecolumn_original.merge(dataframecolumn_compare,on="Key",how="left")
+
+combined_dataframe['Score']=partial_match_vector(combined_dataframe['Match'],combined_dataframe['Compare'])
+
+combined_dataframe['Cenzored']=partial_cenzo_vector(combined_dataframe['Original'], combined_dataframe['Score'])
+
+print(combined_dataframe)
